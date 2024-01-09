0c0ed3e2a1711ef9c0cfb3716cc80934e04e1e90
sergejmehonosin <sergey01051997@mail.ru>
2024-01-09 10:18:51 +0300

    Добавление изменений

--- src/main/kotlin/task_9_3/Main.kt
+++ src/main/kotlin/task_9_3/Main.kt
@@ -21,4 +21,29 @@ fun main() {
     val count = readLine()!!.toInt()
     val str: String = "$text, $name! \n".repeat(count)
     println(str)
+
+    Dog("123",3)
+}
+
+
+class Dog(name: String) {
+    val name = name
+    var age = 10
+
+    init {
+        println("1")
+    }
+
+    init {
+        println("2")
+    }
+
+    constructor(name: String, age: Int) : this(name) {
+        println("constructor")
+        this.age = age
+    }
+
+    constructor(name: String, lastName: String) : this(name) {
+
+    }
 }
